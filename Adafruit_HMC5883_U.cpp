f8371f7825c59fd874ade4dce2812511d3481063
Clik <florian.cliquet@gmail.com>
2016-02-28 23:15:39 +0100

    Remove Wire.begin()
    Wire.begin() should only be called once.
    One some platform, it need arguments, so let the user call it.

--- Adafruit_HMC5883_U.cpp
+++ Adafruit_HMC5883_U.cpp
@@ -155,9 +155,6 @@ Adafruit_HMC5883_Unified::Adafruit_HMC5883_Unified(int32_t sensorID) {
 /**************************************************************************/
 bool Adafruit_HMC5883_Unified::begin()
 {
-  // Enable I2C
-  Wire.begin();
-
   // Enable the magnetometer
   write8(HMC5883_ADDRESS_MAG, HMC5883_REGISTER_MAG_MR_REG_M, 0x00);
   
